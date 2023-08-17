6a3a8749a7e29533980c78471d9167a819c67e46
Swapnil Tripathi <swapnil06.st@gmail.com>
2023-08-17 12:19:15 +0530

    add navigation

---
+++ uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package org.hyperledger.ariesvcx
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val icon: ImageVector,
+)
--- uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/MainActivity.kt
+++ uniffi_aries_vcx/demo/app/src/main/java/org/hyperledger/ariesvcx/MainActivity.kt
@@ -1,81 +1,161 @@
 package org.hyperledger.ariesvcx
 
+import android.R
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.system.Os
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import org.hyperledger.ariesvcx.ui.theme.DemoTheme
+
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Os.setenv("EXTERNAL_STORAGE", this.filesDir.absolutePath, true);
+
         setContent {
-            var walletConfigState by remember {
-                var walletConfig = WalletConfig(
-                    walletName = "test_create_wallet_add_uuid_here",
-                    walletKey = "8dvfYSt5d1taSd6yJdpjq4emkwsPDDLYxkNFysFD2cZY",
-                    walletKeyDerivation = "RAW",
-                    walletType = null,
-                    storageConfig = null,
-                    storageCredentials = null,
-                    rekey = null,
-                    rekeyDerivationMethod = null
-                )
-                mutableStateOf(walletConfig)
-            }
-            val snackbarHostState = remember { SnackbarHostState() }
-            val scope = rememberCoroutineScope()
-            Scaffold(Modifier.fillMaxSize(), snackbarHost = {
-                SnackbarHost(snackbarHostState) { data ->
-                    Snackbar(
-                        modifier = Modifier
-                            .padding(12.dp)
-                    ) {
-                        Text(data.visuals.message)
+            DemoTheme {
+                val navController = rememberNavController()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text("Demo")
+                        })
+                    },
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Connect",
+                                    route = "connect",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Settings",
+                                    route = "settings",
+                                    icon = Icons.Default.Settings,
+                                )
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
                     }
-                }
-            }) {
-                Column(
-                    modifier = Modifier
-                        .padding(it)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ){
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        newIndyProfile(walletConfigState)
-                        scope.launch {
-                            snackbarHostState.showSnackbar("New Indy profile: ${walletConfigState}")
-                        }
-                    }) {
-                        Text("Create new profile")
-                    }
+                ) {
+                    Navigation(navController = navController)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "connect") {
+        composable("connect") {
+            ConnectScreen()
+        }
+        composable("settings") {
+            SettingsScreen()
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.Black,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(
+                        horizontalAlignment = CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.name,
+                        )
+                        if (selected) {
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                color = Color.White,
+                                fontSize = 10.sp
+                            )
+                        } else {
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                color = Color.Gray,
+                                fontSize = 10.sp
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun ConnectScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Connect")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Settings")
     }
 }
